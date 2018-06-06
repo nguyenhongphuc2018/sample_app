9b6db969054632640df113fca14627d66c495e4a
nguyenhongphuc2018 <nguyen.hong.phuc@framgia.com>
2018-06-06 15:20:15 +0700

    Chapter 7 : Sign Up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,26 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = t "home_page.welcome"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+      :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
-  before_save{self.email = email.downcase}
-  validates :name, presence: true, length: {maximum: Settings.name.max}
+  before_save :email_downcase
+  validates :name, presence: true, length: {maximum: Settings.username.maximum}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: Settings.email.max},
+  validates :email, presence: true, length: {maximum: Settings.email.maximum},
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length: {minimum: Settings.pass.min}
+  validates :password, presence: true,
+                    length: {minimum: Settings.pass.minimum}
+
+  private
+  def email_downcase
+    email.downcase!
+  end
 end
