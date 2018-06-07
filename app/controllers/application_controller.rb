6f326a88f4b9de68debbede2a63e10bc3e5945d5
nguyenhongphuc2018 <nguyen.hong.phuc@framgia.com>
2018-06-07 15:06:45 +0700

    Chapter 8 : Basic Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
 
   def not_found
     raise ActiveRecord::RecordNotFound, "Not Found"
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,15 @@ class User < ApplicationRecord
   validates :password, presence: true,
     length: {minimum: Settings.pass.minimum}
 
+  def self.digest string
+    cost =  if ActiveModel::SecurePassword.min_cost
+              BCrypt::Engine::MIN_COST
+            else
+              BCrypt::Engine.cost
+            end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
   private
   def email_downcase
     email.downcase!
